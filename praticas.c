a9dd463369ff1804517130ab5b32a5487ff9abba
Gabriel Auxêncio <115053779+GabrielAuxencio@users.noreply.github.com>
2024-06-23 18:38:43 -0300

    doar_para_vitimas()

--- praticas.c
+++ praticas.c
@@ -18,7 +18,7 @@ int main() {
 	int op;
 	
 	do {
-	printf("Menu de Monitoramento de Chuvas no Rio Grande do Sul\n1. Verificar a PrevisÃ£o do tempo\n2. Doar para vÃ­timas das chuvas\n3. Enviar mensagens de apoio\n4. Sair\n\n>> ");
+	printf("Menu de Monitoramento de Chuvas no Rio Grande do Sul\n1. Verificar a Previsão do tempo\n2. Doar para vítimas das chuvas\n3. Enviar mensagens de apoio\n4. Sair\n\n>> ");
     scanf("%d", &op);	
     
     switch(op){
@@ -26,7 +26,7 @@ int main() {
     	case 2: doar_para_vitimas(); break; 
     	case 3: enviar_mensagens_de_apoio(); break;
     	case 4:  printf("Saindo...\n"); break;
-    	default: printf("OpÃ§Ã£oo invalida! Por favor, escolha uma opÃ§Ã£o valida.\n");
+    	default: printf("Opção invalida! Por favor, escolha uma opção valida.\n");
 	}
     } while(op != 4);
 }
@@ -39,7 +39,7 @@ void verificar_previsao_tempo() {
     scanf("%s", cidade);
     printf("Informe o dia: ");
     scanf("%d", &dia);
-    printf("Informe o mï¿½s: ");
+    printf("Informe o mês: ");
     scanf("%d", &mes);
 
     // Previsï¿½es fictï¿½cias para o mï¿½s de maio
@@ -74,29 +74,28 @@ void salvar_previsao(const char *cidade, const char *previsao) {
 void doar_para_vitimas() {
     int opcao;
 
-    printf("Informaï¿½ï¿½es sobre como doar:\n");
+    printf("\nInformações sobre como doar:\n");
     printf("1. Doar dinheiro\n");
-    printf("2. Doar alimentos\n");
-    printf("3. Doar roupas e cobertores\n");
-    printf("4. Ser voluntï¿½rio\n");
-    printf("Escolha uma opï¿½ï¿½o: ");
+    printf("2. Doar itens\n");
+    printf("3. Ser voluntario\n\n");
+    printf(">> ");
     scanf("%d", &opcao);
 
     switch(opcao) {
         case 1:
-            printf("Para doar dinheiro, vocï¿½ pode fazer uma transferï¿½ncia para a conta do Instituto XYZ - Banco ABC, Agï¿½ncia 0001, Conta 12345-6.\n");
+            printf("Para doar dinheiro, você pode seguir este tutorial do site do Governo do Estado do Rio Grande do Sul:\nhttps://www.rs.gov.br/carta-de-servicos/servicos?servico=1798\n\n");
             break;
         case 2:
-            printf("Para doar alimentos, dirija-se ao posto de arrecadaï¿½ï¿½o mais prï¿½ximo ou entre em contato pelo telefone (51) 1234-5678.\n");
+            printf("Para doar itens em Salvador, dirija-se ao seguintes postos de arrecadação:\n\n"
+			"Unidades da Polícia Militar\nTodas as unidades da PMBA, operacionais e administrativas funcionarão como pontos de arrecadação da campanha.\nSerão aceitas doações de alimentos não perecíveis integrantes da cesta básica, bem como água mineral, fraldas descartáveis para adultos e crianças, agasalhos, cobertores, mantas e itens de higiene.\n\n"
+			"Correios\nA estatal está recebendo e transportando gratuitamente alimentos da cesta básica, material de higiene pessoal, material de limpeza seco, roupas de cama e de banho e ração para pet – sem custo aos doadores.\n\n"
+			"CODESAL\nA sede da Defesa Civil de Salvador (CODESAL) na Bonocô está recebendo doações para ajudar as famílias do Rio Grande do Sul pela campanha Ação Salvador Solidária.\nÁgua mineral e produtos de limpeza, como água sanitária e detergente, podem ser entregues na CODESAL das 8h às 16h\n\n");
             break;
         case 3:
-            printf("Para doar roupas e cobertores, entregue as doaï¿½ï¿½es na sede da organizaï¿½ï¿½o ABC na Rua das Flores, 123.\n");
-            break;
-        case 4:
-            printf("Para ser voluntï¿½rio, acesse o site www.voluntariosabc.org.br e faï¿½a seu cadastro.\n");
+            printf("Para ser voluntário, a Coordenadoria Estadual de Proteção e Defesa Civil do Rio Grande do Sul lançou um formulário para voluntários que desejam atuar em tarefas de organização, seleção e triagem das doações de ajuda humanitária. Instituições, empresas e ou grupos interessados no voluntariado também podem preencher o cadastro no link:\nhttps://casamilitar-rs.com.br/voluntariado/.\n\n");
             break;
         default:
-            printf("Opï¿½ï¿½o invï¿½lida! Por favor, escolha uma opï¿½ï¿½o vï¿½lida.\n");
+            printf("Opção inválida! Por favor, escolha uma opção válida.\n\n");
     }
 }
 
@@ -104,7 +103,7 @@ void enviar_mensagens_de_apoio() {
     int tipo;
 
     printf("Escolha o tipo de cidadï¿½o para enviar a mensagem de apoio:\n");
-    printf("1. Crianï¿½a\n");
+    printf("1. Crianca\n");
     printf("2. Jovem\n");
     printf("3. Adulto\n");
     printf("Escolha uma opï¿½ï¿½o: ");
@@ -112,15 +111,15 @@ void enviar_mensagens_de_apoio() {
 
     switch(tipo) {
         case 1:
-            printf("Mensagem para Crianï¿½a: 'Oi, amiguinho! Fique tranquilo, tudo vai ficar bem. Estamos todos juntos para ajudar!'\n");
+            printf("Mensagem para Crianca: 'Oi, amiguinho! Fique tranquilo, tudo vai ficar bem. Estamos todos juntos para ajudar!'\n");
             break;
         case 2:
             printf("Mensagem para Jovem: 'Ei, fique firme! Sabemos que ï¿½ difï¿½cil, mas sua forï¿½a faz a diferenï¿½a. Conte conosco!'\n");
             break;
         case 3:
-            printf("Mensagem para Adulto: 'Olï¿½, sabemos que esses momentos sï¿½o desafiadores, mas a uniï¿½o faz a forï¿½a. Estamos aqui para apoiar vocï¿½!'\n");
+            printf("Mensagem para Adulto: 'Ola, sabemos que esses momentos sï¿½o desafiadores, mas a uniï¿½o faz a forï¿½a. Estamos aqui para apoiar vocï¿½!'\n");
             break;
         default:
-            printf("Opï¿½ï¿½o invï¿½lida! Por favor, escolha uma opï¿½ï¿½o vï¿½lida.\n");
+            printf("Opcao invalida! Por favor, escolha uma opï¿½ï¿½o vï¿½lida.\n");
     }
 }
